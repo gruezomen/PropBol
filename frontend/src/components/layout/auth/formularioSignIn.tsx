a24a927464c36514d0b9e01528968fbdf7b94c15
FranciscoKilibarda <panchitokili555@gmail.com>
2026-04-08 12:33:09 -0400

    fix:

--- frontend/src/components/layout/auth/formularioSignIn.tsx
+++ frontend/src/components/layout/auth/formularioSignIn.tsx
@@ -367,6 +367,7 @@ export default function LoginForm() {
           <input
             type="email"
             required
+            autoFocus
             placeholder="Ingresa tu correo electrónico"
             value={correo}
             onChange={(e) => {
@@ -393,6 +394,7 @@ export default function LoginForm() {
                 setPassword(e.target.value)
                 validate('password', e.target.value)
               }}
+              onBlur = {() => setShowPassword(false)}
               className="w-full rounded-md border border-gray-300 px-3 py-2 pr-10 text-sm outline-none focus:border-orange-500"
             />
 
