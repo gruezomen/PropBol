c7313ed88800df5e2b11846cb8b02db7665f7234
Jhon Aldo  Fernandez V <149020040+gruezomen@users.noreply.github.com>
2026-04-08 20:32:03 -0400

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -44,7 +44,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: [env.FRONTEND_URL, 'http://localhost:3000', 'http://localhost:3001'],
+    origin: [env.FRONTEND_URL, 'http://localhost:3000', 'http://localhost:3001','https://prop-bol-frontend-beryl.vercel.app'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
